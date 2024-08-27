2420453e9070082070e35bb5bac96d3e249ef95a
mcdd0506 <mcdd0506@gmail.com>
2024-08-28 00:02:17 +0800

    :tada: feat: swap demo and init mcdd

---
+++ src/mcdd.cpp
@@ -0,0 +1,9 @@
+//
+// Created by cbq on 2024/8/27.
+//
+#include <cstdio>
+
+int main() {
+
+    return 0;
+}
--- src/swap_demo.cpp
+++ src/swap_demo.cpp
@@ -3,7 +3,69 @@
 //
 #include <cstdio>
 
+void swap1(int a, int b);
+void swap2(int *a, int *b);
+void swap3(int &a, int &b);
+
 int main() {
-    printf("Hello, World!");
+    int a = 10;
+    int b = 20;
+    printf("===================== swap1 start =======================\n");
+    printf("[main-before] : a=%d, b=%d\n", a, b);
+    swap1(a, b);
+    printf("[main-after] : a=%d, b=%d\n", a, b);
+    printf("===================== swap1 end =========================\n\n");
+
+    printf("===================== swap2 start =======================\n");
+    printf("[main-before] : a=%d, b=%d\n", a, b);
+    swap2(&a, &b);
+    printf("[main-after] : a=%d, b=%d\n", a, b);
+    printf("===================== swap2 end =========================\n\n");
+
+    printf("===================== swap3 start =======================\n");
+    printf("[main-before] : a=%d, b=%d\n", a, b);
+    swap3(a, b);
+    printf("[main-after] : a=%d, b=%d\n", a, b);
+    printf("===================== swap3 end =========================\n\n");
     return 0;
+}
+
+
+/**
+ *
+ * @param a a
+ * @param b b
+ */
+void swap1(int a, int b) {
+    printf("[swap1-before] : a=%d, b=%d\n", a, b);
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("[swap1-after] : a=%d, b=%d\n", a, b);
+}
+
+/**
+ *
+ * @param a &a
+ * @param b &b
+ */
+void swap2(int *a, int *b) {
+    printf("[swap1-before] : a=%d, b=%d\n", *a, *b);
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("[swap1-after] : a=%d, b=%d\n", *a, *b);
+}
+
+/**
+ *
+ * @param a a ref
+ * @param b b ref
+ */
+void swap3(int &a, int &b) {
+    printf("[swap1-before] : a=%d, b=%d\n", a, b);
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("[swap1-after] : a=%d, b=%d\n", a, b);
 }
